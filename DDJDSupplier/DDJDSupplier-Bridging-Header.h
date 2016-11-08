8ef5fa89925aee4a102a7f6268e4b0bc9da41789
xxpenghao1-ios <xxpenghao1@sina.com>
2016-11-08 09:11:11 +0800

    Removed the stupid strange file that shouldn't be tracked

---
+++ DDJDSupplier/DDJDSupplier-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  DDJDSupplier
+//
+//  Created by penghao on 16/11/8.
+//  Copyright © 2016年 彭浩. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
